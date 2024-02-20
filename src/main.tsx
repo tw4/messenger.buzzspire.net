49bb3752e0a3d540c909bf2fad2e524c755e590e
tw4 <88425310+tw4@users.noreply.github.com>
2024-02-20 20:58:13 +0300

    feat: custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,21 @@ import ReactDOM from 'react-dom/client'
 import './assets/base.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routers'
+import { ConfigProvider, theme } from 'antd';
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
+
+<React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#00FF8D',
+        },
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
   </React.StrictMode>
 )
