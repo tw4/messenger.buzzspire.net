0e4c27e06ea2cb0e0bdde743c9cd528c172c47a3
tw4 <88425310+tw4@users.noreply.github.com>
2024-02-20 20:37:44 +0300

    refactor: register and login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,78 +1,83 @@
-import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { Alert, Button, Checkbox, Flex, Form, Input } from 'antd'
-import { LoginForm } from '../Types/FormsType'
-import { LoginRequest } from '../API/Auth'
-import { useState } from 'react'
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Alert, Button, Card, Checkbox, Flex, Form, Image, Input } from 'antd';
+import { LoginForm } from '../Types/FormsType';
+import { LoginRequest } from '../API/Auth';
+import { useState } from 'react';
 
 export const Login: React.FC = () => {
-  const [isError, setIsError] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onFinish = (values: LoginForm): void => {
-    setIsLoading(true)
+    setIsLoading(true);
     LoginRequest(values.UserName, values.Password).then((res) => {
-      const regex = /^[\w-]+\.[\w-]+\.[\w-]+$/g.test(res.token)
+      const regex = /^[\w-]+\.[\w-]+\.[\w-]+$/g.test(res.token);
       if (regex) {
-        localStorage.setItem('username', values.UserName)
-        localStorage.setItem('token', res.token)
-        window.location.href = '/home'
+        localStorage.setItem('username', values.UserName);
+        localStorage.setItem('token', res.token);
+        window.location.href = '/home';
       } else {
-        setIsLoading(false)
-        setIsError(true)
+        setIsLoading(false);
+        setIsError(true);
       }
-    })
-  }
+    });
+  };
 
   return (
     <Flex justify="center" vertical={true} align="center" style={{ height: '100vh' }}>
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        style={{ width: '50%' }}
-      >
-        <Form.Item
-          name="UserName"
-          rules={[{ required: true, message: 'Please input your Username!' }]}
+      <Card title="Login" hoverable={true}>
+        <Flex justify='center' align='center' style={{margin: '20px'}}>
+          <Image src='/logo.svg'  width={50}  />
+        </Flex>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="Password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="Remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
+          <Form.Item
+            name="UserName"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
           </Form.Item>
-        </Form.Item>
+          <Form.Item
+            name="Password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="Remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form.Item>
 
-        {isError && (
-          <Alert
-            style={{ margin: '10px' }}
-            message="Error"
-            description="Username or Password is incorrect"
-            type="error"
-            showIcon
-          />
-        )}
+          {isError && (
+            <Alert
+              style={{ margin: '10px' }}
+              message="Error"
+              description="Username or Password is incorrect"
+              type="error"
+              showIcon
+            />
+          )}
 
-        <Form.Item>
-          <Flex gap="small" align="center">
-            <Button loading={isLoading} type="primary" htmlType="submit" className="login-form-button">
-              Log in
-            </Button>
-            Or <a href="/register">register now!</a>
-          </Flex>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Flex gap="small" align="center">
+              <Button loading={isLoading} type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+              Or <a href="/register">register now!</a>
+            </Flex>
+          </Form.Item>
+        </Form>
+      </Card>
+
     </Flex>
-  )
-}
+  );
+};
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,77 +1,82 @@
-import { Alert, Button, Flex, Form, Input } from 'antd'
-import { RegisterForm } from '../Types/FormsType'
-import { RegisterRequest } from '../API/Auth'
-import { useState } from 'react'
+import { Alert, Button, Card, Flex, Form, Image, Input } from 'antd';
+import { RegisterForm } from '../Types/FormsType';
+import { RegisterRequest } from '../API/Auth';
+import { useState } from 'react';
 
 export const Register: React.FC = () => {
-  const [isError, setIsError] = useState(false)
-  const [errorMessages, setErrorMessages] = useState<string>()
-  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false);
+  const [errorMessages, setErrorMessages] = useState<string>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const onFinish = (values: RegisterForm): void => {
-    setIsLoading(true)
+    setIsLoading(true);
     RegisterRequest(values.UserName, values.Password, values.FullName).then((res) => {
-      const regex = /^[\w-]+\.[\w-]+\.[\w-]+$/g.test(res.token)
+      const regex = /^[\w-]+\.[\w-]+\.[\w-]+$/g.test(res.token);
       if (regex) {
-        localStorage.setItem('username', values.UserName)
-        localStorage.setItem('token', res.token)
-        window.location.href = '/home'
+        localStorage.setItem('username', values.UserName);
+        localStorage.setItem('token', res.token);
+        window.location.href = '/home';
       } else {
-        setIsLoading(false)
-        setErrorMessages(res.error)
-        setIsError(true)
+        setIsLoading(false);
+        setErrorMessages(res.error);
+        setIsError(true);
       }
-    })
-  }
+    });
+  };
 
   return (
     <Flex justify="center" vertical={true} align="center" style={{ height: '100vh' }}>
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        style={{ width: '50%' }}
-      >
-        <Form.Item
-          name="UserName"
-          rules={[{ required: true, message: 'Please input your Username!', min: 3, max: 20 }]}
+      <Card title="Register" hoverable={true}>
+        <Flex justify='center' align='center' style={{margin: '20px'}}>
+          <Image src='/logo.svg'  width={50}  />
+        </Flex>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="Password"
-          rules={[{ required: true, message: 'Please input your Password!', min: 6, max: 20 }]}
-        >
-          <Input type="password" placeholder="Password" />
-        </Form.Item>
+          <Form.Item
+            name="UserName"
+            rules={[{ required: true, message: 'Please input your Username!', min: 3, max: 20 }]}
+          >
+            <Input placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="Password"
+            rules={[{ required: true, message: 'Please input your Password!', min: 6, max: 20 }]}
+          >
+            <Input type="password" placeholder="Password" />
+          </Form.Item>
 
-        <Form.Item
-          name="FullName"
-          rules={[{ required: true, message: 'Please input your Full Name!', min: 3, max: 20 }]}
-        >
-          <Input placeholder="Full Name" />
-        </Form.Item>
+          <Form.Item
+            name="FullName"
+            rules={[{ required: true, message: 'Please input your Full Name!', min: 3, max: 20 }]}
+          >
+            <Input placeholder="Full Name" />
+          </Form.Item>
 
-        {isError && (
-          <Alert
-            style={{ margin: '10px' }}
-            message="Error"
-            description={errorMessages}
-            type="error"
-            showIcon
-          />
-        )}
+          {isError && (
+            <Alert
+              style={{ margin: '10px' }}
+              message="Error"
+              description={errorMessages}
+              type="error"
+              showIcon
+            />
+          )}
 
-        <Form.Item>
-          <Flex gap="small" align="center">
-            <Button loading={isLoading} type="primary" htmlType="submit" className="login-form-button">
-              Register
-            </Button>
-            Or <a href="/">login now!</a>
-          </Flex>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Flex gap="small" align="center">
+              <Button loading={isLoading} type="primary" htmlType="submit" className="login-form-button">
+                Register
+              </Button>
+              Or <a href="/">login now!</a>
+            </Flex>
+          </Form.Item>
+        </Form>
+      </Card>
+
     </Flex>
-  )
-}
+  );
+};
